7bce2e0f6888e62bf631efa523b8d91892645d80
ALPHISTUBE <65537475+ALPHISTUBE@users.noreply.github.com>
2023-11-02 20:05:34 +0600

    Update data.jsx and cleaning the code

--- src/componant/data-load/data.jsx
+++ src/componant/data-load/data.jsx
@@ -1,4 +1,3 @@
-import { element } from 'prop-types';
 import { useEffect, useState } from 'react';
 import Bookmark from '../Bookmark/Bookmark';
 import Product from '../Product/Product';
@@ -10,13 +9,10 @@ const Data = () => {
         .then(res=> res.json())
         .then(data => setValue(data))
     },[])
-    const handle =(props)=>{
-        console.log(props.element)
-    }
     return (
         <div>
             {
-                value.map(element => <Product key={element.id} element ={element} handle = {handle} ></Product>)
+                value.map(element => <Product key={element.id} element ={element}></Product>)
             }
             {
                 value.map(element2 => <Bookmark key={element2.id} element ={element2}></Bookmark>)
