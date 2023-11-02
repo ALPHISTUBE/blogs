cdfeff095d2caaae66cbebc213873df6f84b63b8
ALPHISTUBE <65537475+ALPHISTUBE@users.noreply.github.com>
2023-11-02 20:24:55 +0600

    Removed <newbookmark> in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import Nav from './nav/nav'
 import Data from './componant/data-load/data'
 import { useEffect, useState } from 'react'
-import Newbookmark from './componant/Bookmark/newbookmark'
 
 function App() {
 
@@ -12,7 +11,6 @@ function App() {
       <Nav></Nav>
       <div className='grid'>
       <Data ></Data>
-      <Newbookmark></Newbookmark>
       </div>
     </div>
   )
